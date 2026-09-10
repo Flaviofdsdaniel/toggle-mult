fcac225a8fbbaaf0eefbb3e7c97bca1e4dafcc04
Flaviofdsdaniel <flaviofdsdaniel@gmail.com>
2026-09-10 14:11:01 -0300

    Corrigindo codigo trust policy

--- terraform/github-oidc.tf
+++ terraform/github-oidc.tf
@@ -44,7 +44,7 @@ data "aws_iam_policy_document" "github_actions_trust" {
       variable = "token.actions.githubusercontent.com:sub"
 
       values = [
-        "repo:Flaviofdsdaniel/ToggleMult:ref:refs/heads/main"
+        "repo:Flaviofdsdaniel@49292830/ToggleMult@1301089250:ref:refs/heads/main"
       ]
     }
   }
